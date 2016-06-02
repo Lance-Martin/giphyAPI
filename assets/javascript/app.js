baeaf578a454242fed7c901c3a27dba2398b99c0
Lance Martin <ldmartin7777@gmail.com>
2016-06-02 09:51:41 -0500

    revised styling. Still need to fix half of gifs not playing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   var buttons = ["Dirt Biking", "Kayaking", "Base Jumping", "Skydiving", "Surfing",
 "skating","BMX","Rally Cross", "Snow boarding","Wake Boarding"];
 
+//function to create buttons. Empties the div with an id of "buttons". Loops through the buttons array. Uses jQuery to creat a button, then adds a class of sport, a data-name attribute that corresponds to its index in the buttons array. Adds text into the button that corresponds to its index in the buttons array. Appends the button to the div with an id of "buttons". Calls the clicks function so that buttons can be clicked.
   function renderButtons() {
     $('#buttons').empty();
     for (var i = 0; i < buttons.length; i++){
@@ -14,6 +15,7 @@ $(document).ready(function(){
     clicks();
   }
 
+//When the user clicks the button "Add Sport" then take the string in the text input and push it into the buttons array. Then call the renderButtons() function to create the new button.
   $('#addSport').on('click', function(){
 		var sport = $('#sport-input').val().trim();
 		buttons.push(sport);
@@ -21,6 +23,7 @@ $(document).ready(function(){
 		return false;
 	 });
 
+//function to pause and play gifs. If the clicked gif has a src that equals the gifs data-still attribute then remove the current src attribute and replace it with a src attribute equal to the gifs data-animate attribute. Else set the src attribute back to the data-still attribute.
 function imgClick() {
   $('img').on('click',function(){
     console.log('image has been clicked');
@@ -38,12 +41,12 @@ function imgClick() {
   });
 }
 
+//function to call to dynamically call the on click for buttons. When the user clicks a button, empty the div with an id of "display". Create a variable p that is equal to the clicked buttons data-name attribute. Create a variable queryURL that is the link to the giphyAPI and has the variable p passed into it's search query. Use jQuery to call ajax. Pass ajax queryURL, tell it to 'GET' a JSON object. When the object have been retrieved create a variable results that is equal to the object.data key. Loop through results. If results.rating has a rating of pg-13 or r then do nothing. Else create a variable gifDiv that is equal to a jQuery div object with a class of "item" and "col-md-4". Create variable rating and set it equal to the current index of results.rating. Create variable p and set it equal to jQuery created p tag with a text of '"rating"+ the rating variable'.l
 function clicks() {
    $('button').on('click', function(){
     console.log("clicking");
-    $(this).removeClass('active');
-    $(this).addClass('active');
-    var p = $(this).data('name'); // <------------------------- 1. What is this in "this" case?
+    $('#display').empty();
+    var p = $(this).data('name');
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
     $.ajax({url: queryURL, method: 'GET'})
      .done(function(response) {
@@ -57,17 +60,16 @@ function clicks() {
             var gifDiv = $('<div class="item col-md-4">');
              var rating = results[i].rating;
              var p = $('<p>').text( "Rating: " + rating);
-             var personImage = $('<img style="width: 100%; height: 200px;">');
-             personImage.attr('src', results[i].images.fixed_height_still.url);
-             personImage.attr('data-animate',results[i].images.fixed_height.url);
-             personImage.attr('data-still', results[i].images.fixed_height_still.url);
+             var gifImage = $('<img style="width: 100%; height: 200px;">');
+             gifImage.attr('src', results[i].images.fixed_height_still.url);
+             gifImage.attr('data-animate',results[i].images.fixed_height.url);
+             gifImage.attr('data-still', results[i].images.fixed_height_still.url);
              gifDiv.append(p);
-             gifDiv.append(personImage);
+             gifDiv.append(gifImage);
              $('#display').prepend(gifDiv);
             }
             imgClick();
          }
-
      });
    });
 }
